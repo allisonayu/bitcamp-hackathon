41ead808e0cdf2d1d9515ef8a5e92434c9c47a52
siennarg <152990020+siennarg@users.noreply.github.com>
2025-04-12 21:07:42 +0000

    added_home_page

--- my-react-app/src/Home.tsx
+++ my-react-app/src/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import imageHome from '/workspaces/bitcamp-hackathon/my-react-app/res/grocery icons/home.png'
 import './Home.css'
 
 function Home() {
   return (
-    <div className="container">
-      <h1>Welcome to Buttercup Corner</h1>
+    <div className="container"> 
+        <img src = {imageHome} style={{width:1250,height:1080}} className="my-photo" alt="Centered image"></img>
+        <h1> Welcome to Buttercup Corner</h1>
     </div>
   )
 }
