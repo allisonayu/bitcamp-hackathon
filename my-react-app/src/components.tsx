e2c8ef06254d7ad21bd39324cca7f7c8d4ae2ebe
Allison <allison.a.yu@gmail.com>
2025-04-12 12:01:53 -0500

    added navbar; will fix positioning later

---
+++ my-react-app/src/components.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><a href="www.google.com">home</a></li>
+                <li><a href="www.google.com">create avatar</a></li>
+            </ul>
+        </nav>
+    )
+}
