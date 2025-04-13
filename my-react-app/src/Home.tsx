5f427ab5e41b03e9174ebe3cdb85cfc6b22bd9af
Allison <allison.a.yu@gmail.com>
2025-04-12 20:29:56 -0400

    fixed home page background image + title

--- my-react-app/src/Home.tsx
+++ my-react-app/src/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import imageHome from 'C:/Users/allis/OneDrive/Documents/bitcamp-2025/my-react-app/res/grocery icons/home.png'
 import './Home.css'
+import imageHome from 'C:/Users/allis/OneDrive/Documents/bitcamp-2025/my-react-app/res/grocery icons/home.png'
 
 function Home() {
   return (
-    <div className="container"> 
-        <img src = {imageHome} className="my-photo" alt="Centered image"></img>
-        <h1> Welcome to Buttercup Corner</h1>
+    <div
+      className="home"
+      style={{ backgroundImage: `url(${imageHome})` }}
+    >
+      <h1>Welcome to Buttercup Corner</h1>
     </div>
   )
 }
